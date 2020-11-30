00e2227ca3b5245e883c69e806bf478bd7f30f17
Pete Parks <codein@mac.com>
2020-11-30 12:07:59 -0800

    stick figure drawing with Turtle

---
+++ Python/lab01-turtle.py
@@ -0,0 +1,64 @@
+from turtle import *
+
+#head
+forward(30)
+left(90)
+forward(30)
+left(90)
+forward(30)
+left(90)
+forward(30)
+left(90)
+
+# Neck
+setposition(-5,-15)
+forward(-35)
+
+#arm
+left(225)
+forward(100)
+left(135)
+forward(15)
+left(45)
+forward(90)
+
+#left waist
+left(-135)
+forward(90)
+
+
+#Left Leg
+left(-45)
+forward(100)
+left(135)
+forward(15)
+left(45)
+forward(90)
+left(-45)
+forward(30)
+
+#right leg
+left(-45)
+forward(90)
+left(45)
+forward(15)
+left(135)
+forward(100)
+
+#right waist
+left(-45)
+forward(90)
+
+
+#right arm
+left(-135)
+forward(90)
+left(45)
+forward(15)
+left(135)
+forward(100)
+left(45)
+forward(30)
+
+
+done()
