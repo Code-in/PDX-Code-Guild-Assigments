305e6f52cdf2a1c96fc67d4c81fd0f1477891360
Pete Parks <codein@mac.com>
2020-11-30 16:02:05 -0800

    grades exercise updated

--- lab03-grades.py
+++ lab03-grades.py
@@ -1,16 +1,29 @@
 
-grade = int(input("Enter grade value between 0-100: "))
-letter_grade = "F"
 
-if grade < 59:
+def ifBetweenGrade(grade):
     letter_grade = "F"
-elif 60 <= grade < 70:
-    letter_grade = "D"   
-elif 70 <= grade < 80:
-     letter_grade = "C"      
-elif 80 <= grade < 90:
-    letter_grade = "B"   
-else:
-    letter_grade = "A" 
 
-print(f"You recieved {letter_grade}")
+    if grade <= 59:
+        letter_grade = "F"
+    elif 60 <= grade < 70:
+        letter_grade = "D"   
+    elif 70 <= grade < 80:
+        letter_grade = "C"      
+    elif 80 <= grade < 90:
+        letter_grade = "B"   
+    else:
+        letter_grade = "A" 
+
+    return letter_grade
+
+running = True
+while running == True:
+    value = input("Enter Y or N if you want to geta letter grade: ")
+
+    if value == "N" or value == "n":
+        running = False
+
+    grade = int(input("Enter grade value between 0-100: "))
+    letter_grade = ifBetweenGrade(grade)
+
+    print(f"You recieved {letter_grade}")
