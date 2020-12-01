0b8002b66d91b8b72e4e49590af140105d39e34f
Pete Parks <codein@mac.com>
2020-12-01 12:25:00 -0800

    fixed location of files in repo

--- Python/lab02-madlib.py
+++ Python/lab02-madlib.py
@@ -14,3 +14,6 @@ place3  = input("Enter a place: ")
 emotion  = input("Enter an emition: ")
  
 print(f"It was during the battle of {noun1} when I was running through a {noun2} when a {noun3} went off right next to my platoon. Our {occupation} yelled for us to {verb} to the nearest {place1} we could find. When we got to the {place2} we {verd_ending_in_ed} to start a fire. As we were starting the fire the enemy saw the {noun4} from the fire and started {verd_ending_in_ing} {plural_noun} at us. we all quickly ducked behind the {noun5} at the {place3} and returned fire. we quickly eliminated the enemy and  were {emotion} that we had won the battle.")
+
+
+
