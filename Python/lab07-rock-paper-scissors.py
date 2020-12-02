6c16e6aa0ef9a8986be3ca787e98f9fb9e31aec7
Pete Parks <codein@mac.com>
2020-12-02 12:37:18 -0800

    Updated game to include Vulcan and Lizard playing options

--- Python/lab07-rock-paper-scissors.py
+++ Python/lab07-rock-paper-scissors.py
@@ -1,46 +1,33 @@
 import random
 
-choices = 'rps'
-names = {"r":"Rock", "p":"Paper", "s":"Scissors"}
-
+choices = 'rpslv'
+names = {"r":"Rock", "p":"Paper", "s":"Scissors", "v":"Volcan", "l":"Lizard"}
+wins = {'rr':0, 'rs':1, 'rp':2, 'rv':2, 'rl':1, 
+        'ss':0, 'sr':2, 'sp':1, 'sv':2, 'sl':1, 
+        'pp':0, 'ps':2, 'pr':1, 'pv':1, 'pl':2, 
+        'vv':0, 'vr':1, 'vp':2, 'vs':1, 'vl':2, 
+        'll':0, 'lr':2, 'lp':1, 'ls':2, 'lv':1}
 
 
 def ask_user_for_rock_paper_scissors():
     response = False
     while response == False:
-        selection = input("Enter 'R' for Rock, 'P' for Paper and 'S' for Scissors: ")
+        selection = input("Enter 'R' for Rock, 'P' for Paper, 'S' for Scissors, 'V' for Volcan and 'L' for Lizard: ")
         if selection.lower() in choices:
             return selection.lower()
-
 
 def ask_computer_for_choice():
     return random.choice(choices)
 
 def who_won(computer, player):
-    if computer == player:
-        ouput = 1
-    elif computer + player == 'rs': # Rock beats Scissors Computer Winds
-        ouput =  2
-    elif computer + player == 'ps': # paper loses to Scissors Player 1 wins
-        ouput =  3
-    elif computer + player == 'rp': # rock loses to paper Player 1 wins
-        ouput =  3
-    elif computer + player == 'sp': # scissors beats paper Computer wins
-        ouput =  2
-    elif computer + player == 'sr': # scissors loses Rock Player 1 wins
-        ouput =  3
-    elif computer + player == 'pr': # paper beats rock Computer wins
-        ouput =  2
-    else:
-        output  = 4
-    return ouput
+    return wins[computer + player]
 
 def print_winner(winner, computer_choice, player_choice):
-    if winner == 1:
+    if winner == 0:
         print(f"Computer: {names[computer_choice]} and Player 1: {names[player_choice]} tied!")
-    elif winner == 2:
+    elif winner == 1:
        print(f"Computer: {names[computer_choice]} wins over Player 1: {names[player_choice]}")
-    elif winner == 3:
+    elif winner == 2:
         print(f"Computer: {names[computer_choice]} loses to Player 1: {names[player_choice]}")
     else:
         print("I missed an option")
@@ -48,7 +35,7 @@ def print_winner(winner, computer_choice, player_choice):
 def main():
     running = True
     while running == True:
-        value = input("Enter Y or N if want play a game of 'Rock, Paper, Scissors': ")
+        value = input("Enter Y or N if want play a game of 'Rock, Paper, Scissors, Vulcan, Lizard': ")
         if value == "N" or value == "n":
             break
         player_choice = ask_user_for_rock_paper_scissors()
