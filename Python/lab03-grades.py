54efddcfa21c3ec15072f4bb39bbdc7a0e1a69e1
Pete Parks <codein@mac.com>
2020-12-04 23:08:52 -0800

    cleaned up my code and improve the functionality with REPL structure

--- Python/lab03-grades.py
+++ Python/lab03-grades.py
@@ -1,6 +1,6 @@
 
-
-def ifbetweengrade(grade):
+# Convert grade from interger to letter grade
+def if_between_grade(grade):
     letter_grade = "F"
 
     if grade <= 59:
@@ -16,43 +16,48 @@ def ifbetweengrade(grade):
 
     return letter_grade
 
-
-def modgrade(grade):
+# Convert letter grade to a +/- letter grade based on there score
+def mod_grade(grade):
     remainder = grade % 10
     higlow = ""
-    if grade < 100:
+
+    if grade <= 59:
+        highlow = "" 
+    elif grade < 100:
         if remainder < 3:
             highlow = "-"
         elif remainder > 7:
             highlow = "+"
         else:
-            highlow = "" 
+            highlow = ""
     else:
-       highlow = "++"  
+       highlow = "++" 
+
 
     return highlow
 
-def inputgrade():
-    grade = input("Enter grade value between 0-100: ")
-    if grade.isdigit():
-        return int(grade)
-    else:
-        grade = inputgrade()
-    return grade
+# Ask the user for a integer grade to work with
+def input_grade():
+    while True:
+        grade = input("Enter grade value between 0-100: ")
+        if grade.isdigit():
+            return int(grade)
 
 
+# Main function where all the methods will be called if the user want to get a letter
 def main():
+
     running = True
     while running == True:
         value = input("Enter Y or N if you want to get a letter grade: ")
-
-        if value == "N" or value == "n":
+        if value in ['n', 'N', 'No', 'NO', 'no']:
             break
+        elif value.isascii and value in ['y', 'Y', 'Yes', 'YES', 'yes']:
+            grade = input_grade()
+            letter_grade = if_between_grade(grade)
+            grade_mod = mod_grade(grade)
+            print(f"You recieved {letter_grade}{grade_mod}")
 
-        grade = inputgrade()
-        letter_grade = ifbetweengrade(grade)
-        grade_mod = modgrade(grade)
 
-        print(f"You recieved {letter_grade}{grade_mod}")
 
 main()
