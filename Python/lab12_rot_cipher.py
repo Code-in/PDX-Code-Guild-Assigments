7b0f30f87b2da939d79dedc125203ea95cb85a71
Pete Parks <codein@mac.com>
2020-12-09 14:53:34 -0800

    Added a larger encode string base

--- Python/lab12_rot_cipher.py
+++ Python/lab12_rot_cipher.py
@@ -18,6 +18,7 @@ def rot_by_offset(text, offset):
         if char in special:
             new_ord = ord(char)
         else:
+
             char_ord = ord(char)
             wip_ord = char_ord + offset
             if  wip_ord > end_ord:
@@ -28,8 +29,62 @@ def rot_by_offset(text, offset):
         out_string += chr(new_ord)
 
     return out_string
+    
 
+
+
+def rot_all_characters_by_offset(chars, offset):
+    encoder = string.ascii_letters + string.punctuation + " "
+    #print(f"Chars: {encoder}")
+    encoder_len = len(encoder)
+    encoder_list = list(encoder)
+
+    encoder_dict_index = {}
+    for i in range(len(encoder_list)):
+        encoder_dict_index[encoder_list[i]] = i
+    
+    encoder_dict_char = {}
+    for i in range(len(encoder_list)):
+        encoder_dict_char[i] = encoder_list[i]
+
+    #print(encoder_dict_index)
+    #print(encoder_dict_char)
+
+    chars_list = list(chars)
+    start_ord = encoder[0]
+    end_ord = encoder[-1]
+    new_ord = 0
+    out_string = ''
+
+    for char in chars_list:
+        if not (char in encoder):
+            new_ord = encoder_dict_index[char]
+        else:
+            if offset > 0:
+                char_ord = encoder_dict_index[char]
+                wip_ord = char_ord + offset
+                if  wip_ord > encoder_dict_index[end_ord]:
+                    r = wip_ord - encoder_dict_index[end_ord]
+                    new_ord = encoder_dict_index[start_ord] + (r - 1)
+                else:
+                    new_ord = wip_ord
+            elif offset < 0:
+                char_ord = encoder_dict_index[char]
+                wip_ord = char_ord + offset
+                if  wip_ord < encoder_dict_index[start_ord]:
+                    r = wip_ord - encoder_dict_index[start_ord]
+                    new_ord = encoder_dict_index[end_ord] + (r + 1)
+                else:
+                    new_ord = wip_ord
+            else:
+               new_ord += new_ord 
+        out_string += encoder_dict_char[new_ord]
+    return out_string
 
 print(rot_by_offset("hello world!", 22))
-print(rot_by_offset("uryyb jbeyq!", 4))
+print(rot_by_offset("dahhk sknhz!", (26 - 22)  ))
+
+print(rot_all_characters_by_offset("hello@world!", 22))
+print(rot_all_characters_by_offset("DAHHKkSKNHz[", -22  ))
+
 
