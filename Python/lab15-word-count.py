cefffb5acf6409fa56b0e813db9f87f120b8f3f1
Pete Parks <codein@mac.com>
2020-12-14 14:46:10 -0800

    made it so it runs both verions

--- Python/lab15-word-count.py
+++ Python/lab15-word-count.py
@@ -18,18 +18,8 @@ def read_in_text():
             word_count_dict[word] += 1
         else:
             word_count_dict[word] = 1
-    print(word_count_dict)
+    #print(word_count_dict)
     return word_count_dict
-
-def version2(word_count_dict):
-        # word_dict is a dictionary where the key is the word and the value is the count
-        words = list(word_count_dict.items()) # .items() returns a list of tuples
-        words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
-        for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
-            print(words[i])
-
-def version3(word, word_count_dict):
-    return word_count_dict[word]
 
 def get_word(word_count_dict):
     while True:
@@ -39,14 +29,33 @@ def get_word(word_count_dict):
         else:
             print(f"There are 0 occurances of the {word} in our text")
 
+def specific_word_count(word, word_count_dict):
+    return word_count_dict[word]
+
+
+def version2(word_count_dict):
+    # word_dict is a dictionary where the key is the word and the value is the count
+    words = list(word_count_dict.items()) # .items() returns a list of tuples
+    words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
+    for i in range(min(len(words), 10)):  # print the top 10 words, or all of them, whichever is smaller
+        print(words[i])
+
+def version3(word_count_dict):
+    while True:
+        word = get_word(word_count_dict)
+        count = specific_word_count(word, word_count_dict)
+        print(f"There are {count} occurances of the {word} in our text")
+
+
+
+
+
 
 def main():
     print("Welcome Word Count 5000")
     word_count_dict = read_in_text()
 
-    while True:
-        word = get_word(word_count_dict)
-        count = version3(word, word_count_dict)
-        print(f"There are {count} occurances of the {word} in our text")
+    version2(word_count_dict)
+    version3(word_count_dict)
 
 main()
