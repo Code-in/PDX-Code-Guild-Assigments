843525fc8f2374eb7d471b8b9efa56c058deea97
Pete Parks <codein@mac.com>
2021-01-08 14:16:37 -0800

    Adding a directory for my HTML and CSS work

---
+++ HTML-CSS/company-lab/config.js
@@ -0,0 +1,6 @@
+
+// config.js
+var config = { // this should be const instead if you're using ES6 standards
+  apiKey : '563492ad6f9170000100000192b35e3f70a94affab03f7c9f1d1420e'
+}
+ /* include in a secrets.js  make sure git ignores that file */
