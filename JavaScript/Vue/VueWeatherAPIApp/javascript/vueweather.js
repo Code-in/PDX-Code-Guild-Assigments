1efb6aa72fe7c7942df71efcd199d0602d10ec58
Pete Parks <codein@mac.com>
2021-02-23 21:15:00 -0800

    Resolved test layout for weather report and removed Count Down Watch until I get to implementing it.

--- JavaScript/Vue/VueWeatherAPIApp/javascript/vueweather.js
+++ JavaScript/Vue/VueWeatherAPIApp/javascript/vueweather.js
@@ -35,6 +35,21 @@ let app = new Vue({
             console.log(datetime) 
             return datetime
         },
+        convertToTime: function (dt) {
+            let unix_timestamp = dt
+            let datetime = new Date(unix_timestamp*1000)
+            var localeSpecificTime = datetime.toLocaleTimeString();
+            return localeSpecificTime.replace(/:\d+ /, ' ');
+        },
+        convertInches: function (rain) {
+            if(rain > 0) {
+                rain = rain.toFixed(2)
+                rain = "" + rain + " in"
+            } else {
+                rain = "0.0 in"
+            }
+            return rain
+        },
         getLocation: function () {
             if (navigator.geolocation) {
                 console.log("getCurrentPosition")
@@ -57,6 +72,12 @@ let app = new Vue({
         updateWeatherData: function() {
             this.day = this.weather
             this.day.dt = this.convertDateTime(this.day.dt)
+            console.log(this.day.dt )
+            this.day.sunrise = this.convertToTime(this.day.sunrise)
+            console.log(this.day.sunrise)
+            this.day.sunset = this.convertToTime(this.day.sunset)
+            console.log(this.day.sunset )
+            this.day.rain = this.convertInches(this.day.rain)
             console.log(this.day.weather[0].icon)
             this.weather_icon = "http://openweathermap.org/img/wn/" + this.day.weather[0].icon + "@4x.png"
         }
