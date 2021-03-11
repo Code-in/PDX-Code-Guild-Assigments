2f73c619953ac54a14d8a301530ed7daa46438d2
Pete Parks <codein@mac.com>
2021-03-10 18:50:22 -0800

    Added pagination to the Pokedex

--- Django/lab04/pokedex/views.py
+++ Django/lab04/pokedex/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Pokemon, PokemonType
+from django.core.paginator import Paginator
 from django.urls import reverse
 # Create your views here.
+
+PAGECOUNT = 60
 
 def index(request):
     pokemon = Pokemon.objects.all()
     pokemon_type = PokemonType.objects.all()
+    pokinator = Paginator(pokemon, PAGECOUNT)
+    page = request.GET.get('page')
+    pokepage = pokinator.get_page(page)
     print(pokemon)
     context = {
-        "pokemon": pokemon,
+        "pokemon": pokepage,
         "pokemon_type": pokemon_type,
-        "loop_times": range(1, 12)
+        "count": PAGECOUNT
     }
     return render(request, 'pokedex/index.html', context)
 
@@ -24,3 +30,5 @@ def pokemon(request, id):
         "pokemon_type": pokemon_type,
     }
     return render(request, 'pokedex/pokemon.html', context)
+
+   # {%url 'pokedex:index'%}
